9902477e3b43843fa7003f71a03f4e09d84aa73b
hitesh-92 <hiteshgohil@hotmail.co.uk>
2019-03-12 20:18:43 +0000

    using HandleFunc for route

--- routing/serveMux/main.go
+++ routing/serveMux/main.go
@@ -5,25 +5,18 @@ import (
 	"net/http"
 )
 
-type home int
-
-func (h home) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+func home(w http.ResponseWriter, req *http.Request) {
 	io.WriteString(w, "Welcome Home!!! This is better!")
 }
 
-type about int
-
-func (a about) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+func about(w http.ResponseWriter, req *http.Request) {
 	io.WriteString(w, "About. Well Theres still alot we dont know about")
 }
 
 func main() {
-	var h home
-	var a about
 
-	mux := http.NewServeMux()
-	mux.Handle("/", h)
-	mux.Handle("/about", a)
+	http.HandleFunc("/", home)
+	http.HandleFunc("/about", about)
 
-	http.ListenAndServe(":8080", mux)
+	http.ListenAndServe(":8080", nil)
 }
