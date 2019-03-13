e71c619c1d2701c1d85b289c8063d2f8b36254bf
hitesh-92 <hiteshgohil@hotmail.co.uk>
2019-03-13 09:01:41 +0000

    added template

--- ex-web-server/main.go
+++ ex-web-server/main.go
@@ -2,11 +2,25 @@ package main
 
 import (
 	"io"
+	"log"
 	"net/http"
+	"text/template"
+	"time"
 )
 
 func root(w http.ResponseWriter, req *http.Request) {
-	io.WriteString(w, "<p>Welcome. Check out <a href='/dog'>dog</a> and <a href='/me'>me</a> </p>")
+	// io.WriteString(w, "<p>Welcome. Check out <a href='/dog'>dog</a> and <a href='/me'>me</a> </p>")
+	tpl, err := template.ParseFiles("index.gohtml")
+
+	if err != nil {
+		log.Fatalln("Error Parsing File")
+	}
+
+	err = tpl.ExecuteTemplate(w, "index.gohtml", time.Now())
+
+	if err != nil {
+		log.Fatalln("Error Executing Template")
+	}
 }
 
 func dog(w http.ResponseWriter, req *http.Request) {
